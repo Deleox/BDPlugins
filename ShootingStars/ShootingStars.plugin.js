85ab2b82457f25ac2abd24812f83bbb8c314e017
Deleox <153164151+Deleox@users.noreply.github.com>
2025-02-02 23:37:51 +1100

    Update ShootingStars.plugin.js

--- ShootingStars/ShootingStars.plugin.js
+++ ShootingStars/ShootingStars.plugin.js
@@ -4,7 +4,7 @@
  * @version 1.0.0
  * @author Deleox001
  * @authorId 1156430974008184962
- * @source https://github.com/Deleox/
+ * @source https://github.com/Deleox/BDPlugins/
  */
 
 class ShootingStars {
